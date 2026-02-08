f9db2debfd1b1202561b5e8e28f02dc37bbab94e
Shreyasdubeyy <shreyasdubey03@gmail.com>
2026-02-08 19:11:59 +0530

    Sorting String Based On Length Using Streams

---
+++ SortingStringBasedOnLengthUsingStreams.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class SortingStringBasedOnLengthUsingStreams {
+    public static void main(String[] args) {
+        List<String> list=new ArrayList<String>();
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter array size");
+	int n=sc.nextInt();
+	sc.nextLine();
+	
+	for(int i=0;i<n;i++) {
+		list.add(sc.nextLine());
+	}
+	
+    System.out.println("Before sorting:"+list);
+    List<String> sortedList = list.stream().sorted(Comparator.comparing(String::length)).collect(Collectors.toList());
+		System.out.println("After sorting:"+sortedList);
+    }
+}
