a036d5b3520a55b9ff6e4e47d020deb74d67f80a
Shreyasdubeyy <shreyasdubey03@gmail.com>
2026-02-14 22:46:17 +0530

    Employee Details Of Highest Salary Using Streams

---
+++ EmployeeDetailsOfHighestSalaryUsingStreams.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeeDetailsOfHighestSalaryUsingStreams{
+    public static void main(String[] args) {
+         List<Employee> list=new ArrayList<>();
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter array size");
+			int n=sc.nextInt();
+			sc.nextLine();
+			
+			for(int i=0;i<n;i++) {
+				
+				System.out.println("Enter id");
+				int id=sc.nextInt();
+				sc.nextLine();
+
+                System.out.println("Enter Name");
+				String name=sc.nextLine();
+
+				System.out.println("Enter salary");
+				double salary=sc.nextDouble();
+				
+				list.add(new Employee(id,name,salary));
+			}
+
+        Employee emp = list.stream().max(Comparator.comparingDouble(Employee::getSalary)).orElseGet(null);
+        System.out.println(emp!=null?"Employee with highest salary: " + emp.getName() + ", Salary: " + emp.getSalary() : "No employees found.");
+    }
+}
